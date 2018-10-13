bab65a0c656e86bf3509dbb00b66492089bbdc2b
Shrey Gupta <shrey.gupta@duke.edu>
2018-10-13 15:47:05 -0400

    Update events

--- datathon/js/chart.js
+++ datathon/js/chart.js
@@ -21,6 +21,24 @@ var topData = [
 var ctx = document.getElementById("topChart").getContext("2d");
 var topChart = new Chart(ctx).Doughnut(topData, options);
 
+var minorityData = [
+    {
+        value: 46,
+        color: "#001A57",
+        highlight: "#0990C3",
+        label: "Underrepresented"
+    },
+    {
+        value: 54,
+        color: "#F7464A",
+        highlight: "#FF5A5E",
+        label: "Non-Minority"
+    }
+]
+
+var ctx = document.getElementById("minorityChart").getContext("2d");
+var minorityChart = new Chart(ctx).Doughnut(minorityData, options);
+
 var satisfactionData = [
     {
         value: 88,
